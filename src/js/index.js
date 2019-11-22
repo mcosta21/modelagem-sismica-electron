37fa80ce8f0252d3e7f5f578b7b1fcf35a7de33a
mcosta21 <marcioc424@gmail.com>
2019-11-21 23:33:22 -0300

    Implementação de gráfico por quantidade de geofones, selectmenu de material(obter velocidade), criação de slicer com range de valores de acordo com material proposto, e alteração no layout.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,82 @@
 var nodeConsole = require('console');
 var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
+const {dialog} = require('electron').remote
+
+let arrayPontos = []
+
+async function gerar(){
+    
+    let geofones = document.getElementById("geofones").value
+    if(geofones.length == 0){
+        const dialogAlert = {title: "Erro", type: 'info', buttons: ['OK'], message: "A quantidade de geofone não foi informada."}
+        dialog.showMessageBox(dialogAlert, (i) => console.log(i))
+        return
+    }
+
+    let distancia = document.getElementById("distancia").value
+    if(distancia.length == 0){
+        const dialogAlert = {title: "Erro", type: 'info', buttons: ['OK'], message: "A distância total não foi informada."}
+        dialog.showMessageBox(dialogAlert, (i) => console.log(i))
+        return
+    }
+
+    let altura = document.getElementById("altura").value
+    if(altura.length == 0){
+        const dialogAlert = {title: "Erro", type: 'info', buttons: ['OK'], message: "A altura não foi informada."}
+        dialog.showMessageBox(dialogAlert, (i) => console.log(i))
+        return
+    }
+
+    let velocidade = document.getElementById("myRange").value
+    if(velocidade.length == 0){
+      const dialogAlert = {title: "Erro", type: 'info', buttons: ['OK'], message: "A velocidade não foi informada."}
+      dialog.showMessageBox(dialogAlert, (i) => console.log(i))
+      return
+    }    
+
+    if(arrayPontos != 0){
+      const dialogAlert = {title: "Erro", type: 'question', buttons: ['Confirmar', 'Cancelar'], message: "Tem certeza de que deseja gerar novamente?"}
+      let response = await dialog.showMessageBox(dialogAlert).then(function(value){
+        return value.response
+      })
+      if(response == 0){
+        //0 - confirmar e 1 - cancelar
+        limparListas()
+      } 
+      else{
+        return
+      }     
+    }
+    
+    let lista = document.getElementById("listaPontos")
+    for(i = 0; i < Number.parseInt(geofones); i++){
+        let geofone = Number.parseFloat(distancia) / (Number.parseInt(geofones) - i)
+
+        let tempo = (2 * Math.sqrt( Math.pow(Number.parseFloat(altura), 2) + (Math.pow(Number.parseFloat(geofone), 2) / 4) ) ) / Number.parseFloat(velocidade)
+        
+        let pontoJson = {
+            "sequencia": arrayPontos.length, 
+            "distancia": Number(geofone), 
+            "altura": altura, 
+            "velocidade": velocidade, 
+            "tempo": tempo
+        }
+
+        arrayPontos.push(pontoJson)
+
+        let ponto =   "<ul class='ponto'>" 
+        + "   <li class='sequencia'> N° " + arrayPontos.length + "</li> "
+        + "   <li class='separador'></li> "
+        + "   <li class='linha'> Distância: <strong>" + geofone + "m</strong> </li> "
+        + "   <li class='linha'> Altura: <strong>" + altura + "m</strong></li> "
+        + "   <li class='linha'> Velocidade: <strong>" + velocidade + "m/s</strong></li> "
+        + "   <li class='linha'> Tempo: <strong>" + tempo + "s</strong></li> "
+        + "</ul>"
+
+        lista.innerHTML += ponto
+    } 
+    calcular()                
+}
 
 function calcular(){
     
@@ -11,96 +88,60 @@ function calcular(){
         alturaTotal += Number(arrayPontos[i].altura)
         tempoTotal += Number(arrayPontos[i].tempo)
     }
-    resultado.innerHTML = "<ul> " 
-                        + "<li> Distância Total: " + distanciaTotal + "cm </li>"
-                        + "<li> Altura Total: " + alturaTotal + "cm </li>"
-                        + "<li> Tempo Total: " + tempoTotal + "s </li>"
+    resultado.innerHTML = "<div class='listaResultados'> " 
+                          + "<h5>Distância Total: </h5>"
+                          + "<h4>" + distanciaTotal + "cm</h4>"
+                          + "<h5> Altura Total: " + alturaTotal + "cm </h5>"
+                          + "<h5> Tempo Total: " + tempoTotal + "s </h5>"
                         + "</div>"
     renderChart()
 }
 
-let arrayPontos = []
+async function limpar(){
 
-function adicionarPonto(){
-    let distancia = document.getElementById("distancia").value
-    if(distancia.length == 0){
-        return
-    }
+    const dialogAlert = {title: "Erro", type: 'question', buttons: ['Confirmar', 'Cancelar'], message: "Tem certeza de que deseja limpar?"}
+    let response = await dialog.showMessageBox(dialogAlert).then(function(value){
+      return value.response
+    })
+    if(response == 1){
+      //0 - confirmar e 1 - cancelar
+      return
+    } 
 
-    let altura = document.getElementById("altura").value
-    if(altura.length == 0){
-        return
-    }
-
-    let material = document.getElementById("material").value
-    let velocidade = obterVelocidade(material)
-
-    let tempo = (2 * Math.sqrt( Math.pow(Number.parseFloat(altura), 2) + (Math.pow(Number.parseFloat(distancia), 2) / 4) ) ) / Number.parseFloat(velocidade)
-
-    let pontoJson = {
-                        "sequencia": arrayPontos.length, 
-                        "distancia": Number(distancia), 
-                        "altura": altura, 
-                        "velocidade": velocidade, 
-                        "tempo": tempo
-                    }
-
-    arrayPontos.push(pontoJson)
-
-    let ponto =   "<ul class='ponto'>" 
-                + "   <li class='sequencia'> N° " + arrayPontos.length + "</li> "
-                + "   <li class='separador'></li> "
-                + "   <li class='linha'> Distância: <strong>" + distancia + "m</strong> </li> "
-                + "   <li class='linha'> Altura: <strong>" + altura + "m</strong></li> "
-                + "   <li class='linha'> Velocidade: <strong>" + velocidade + "m/s</strong></li> "
-                + "   <li class='linha'> Tempo: <strong>" + tempo + "s</strong></li> "
-                + "</ul>"
-
-    let lista = document.getElementById("listaPontos")
-    lista.innerHTML += ponto
-    
-    distancia = document.getElementById("distancia").value = ''
-    altura = document.getElementById("altura").value = ''
-}
-
-function obterVelocidade(material){
-    if(material == 'agua'){
-        return 1500
-    }
-    else if(material == 'sal'){
-        return 19
-    }
-    else{
-        return null
-    }
-}
-
-function limpar(){
     arrayPontos = []
     let lista = document.getElementById("listaPontos")
     lista.innerHTML = ''
     resultado.innerHTML = ''
+    let material = document.getElementById("material").value = ''
+    let slider = document.getElementById("myRange");
+    slider.min = '';
+    slider.max = '';
+    slider.value = '';
+    let valueRange = document.getElementById("valueRange");  
+    valueRange.innerHTML = '';
+    let geofones = document.getElementById("geofones").value = ''
     let distancia = document.getElementById("distancia").value = ''
-	let altura = document.getElementById("altura").value = ''
-	myChart.data.labels = []
-	myChart.data.datasets = []
-	window.myChart.update()
+    let altura = document.getElementById("altura").value = ''
+    myChart.data.labels = []
+    myChart.data.datasets = []
+    window.myChart.update()
+}
+
+function limparListas(){
+  arrayPontos = []
+  let lista = document.getElementById("listaPontos")
+  lista.innerHTML = ''
+  resultado.innerHTML = ''
+  myChart.data.labels = []
+  myChart.data.datasets = []
+  window.myChart.update()
 }
 
 
 function renderChart() {
     myChart.data.datasets = []
-    //let dataDistancia = []
-	//let dataAltura = []
 	let newDataset
-    for(i=0; i < arrayPontos.length; i++){  
-        //dataDistancia.push(0)    
-        //dataAltura.push(0)
-        //dataDistancia.push(arrayPontos[i].distancia / 2)
-        //dataAltura.push(Number(arrayPontos[i].altura)*-1)
-        //dataDistancia.push(arrayPontos[i].distancia)
-		//dataAltura.push(0)    
-		
+    for(i=0; i < arrayPontos.length; i++){  		
 		newDataset = {
 			label: 'N' + (i+1),
 			data: [{x: 0, y: 0}, {x: (arrayPontos[i].distancia / 2), y: (Number(arrayPontos[i].altura)*-1)}, {x: arrayPontos[i].distancia, y: 0}],
@@ -111,30 +152,6 @@ function renderChart() {
 		myChart.data.datasets.push(newDataset)
 		window.myChart.update()
     }       
-
-	/*
-	let newLabels = ['0', '50', '100']
-	
-    let newDataset = {
-        label: 'Dataset ' + 1,
-        data: [(0, 0), (50, -60), (100, 0)],
-        lineTension: 0,
-        borderColor: getRandomColor()
-	};
-	
-	let newDataset2 = {
-        label: 'Dataset ' + 2 ,
-        data: [0, -60, 0],
-		lineTension: 0,
-		backgroundColor: getRandomColor(),
-        borderColor: getRandomColor()
-	};
-	
-	
-	myChart.data.labels.push('120')
-	myChart.data.datasets.push(newDataset2)
-	window.myChart.update()
-	*/
 }
 
 function getRandomColor() {
@@ -177,4 +194,125 @@ window.onload = function() {
 			}
 	});
 }
+
+function changeMaterial(){
+
+  let material = document.getElementById("material").value
+  var slider = document.getElementById("myRange");
+  var valueRange = document.getElementById("valueRange");
+
+  if(material == 'areiaseca'){   
+      slider.min = Number.parseFloat(0.2)      
+      slider.max = Number.parseFloat(1.0)
+      slider.value = Number.parseFloat(0.4)
+  }
+  else if(material == 'areiasaturada'){   
+    slider.min = Number.parseFloat(1.5)      
+    slider.max = Number.parseFloat(2.0)
+    slider.value = Number.parseFloat(1.7)
+  }
+  else if(material == 'argila'){   
+    slider.min = Number.parseFloat(1.0)      
+    slider.max = Number.parseFloat(2.5)
+    slider.value = Number.parseFloat(1.7)
+  }
+  else if(material == 'tillglacial'){   
+    slider.min = Number.parseFloat(1.5)      
+    slider.max = Number.parseFloat(2.5)
+    slider.value = Number.parseFloat(2.0)
+  }
+  else if(material == 'permafroste'){   
+    slider.min = Number.parseFloat(3.5)      
+    slider.max = Number.parseFloat(4.0)
+    slider.value = Number.parseFloat(3.7)
+  } 
+  else if(material == 'arenitos'){   
+    slider.min = Number.parseFloat(2.0)      
+    slider.max = Number.parseFloat(6.0)
+    slider.value = Number.parseFloat(4.0)
+  }
+  else if(material == 'arenitoterciario'){   
+    slider.min = Number.parseFloat(2.0)      
+    slider.max = Number.parseFloat(2.5)
+    slider.value = Number.parseFloat(2.3)
+  }
+  else if(material == 'arenitopennant'){   
+    slider.min = Number.parseFloat(4.0)      
+    slider.max = Number.parseFloat(4.5)
+    slider.value = Number.parseFloat(4.3)
+  }
+  else if(material == 'quartzitocambiano'){   
+    slider.min = Number.parseFloat(5.5)      
+    slider.max = Number.parseFloat(6.0)
+    slider.value = Number.parseFloat(5.7)
+  }
+  else if(material == 'calcarios'){   
+    slider.min = Number.parseFloat(2.0)      
+    slider.max = Number.parseFloat(6.0)
+    slider.value = Number.parseFloat(4.0)
+  }
+  else if(material == 'greda'){   
+    slider.min = Number.parseFloat(2.0)      
+    slider.max = Number.parseFloat(2.5)
+    slider.value = Number.parseFloat(2.3)
+  }
+  else if(material == 'oolitos'){   
+    slider.min = Number.parseFloat(3.0)      
+    slider.max = Number.parseFloat(4.0)
+    slider.value = Number.parseFloat(3.5)
+  }
+  else if(material == 'calcariocarbonifero'){   
+    slider.min = Number.parseFloat(5.0)      
+    slider.max = Number.parseFloat(5.5)
+    slider.value = Number.parseFloat(5.3)
+  }
+  else if(material == 'dolomitos'){   
+    slider.min = Number.parseFloat(2.5)      
+    slider.max = Number.parseFloat(6.5)
+    slider.value = Number.parseFloat(4.5)
+  }
+  else if(material == 'sal'){   
+    slider.min = Number.parseFloat(4.5)      
+    slider.max = Number.parseFloat(5.0)
+    slider.value = Number.parseFloat(4.7)
+  }
+  else if(material == 'anidrita'){   
+    slider.min = Number.parseFloat(4.5)      
+    slider.max = Number.parseFloat(6.5)
+    slider.value = Number.parseFloat(5.5)
+  }
+  else if(material == 'gripso'){   
+    slider.min = Number.parseFloat(2.0)      
+    slider.max = Number.parseFloat(3.5)
+    slider.value = Number.parseFloat(2.7)
+  }
+  else if(material == 'granito'){   
+    slider.min = Number.parseFloat(5.5)      
+    slider.max = Number.parseFloat(6.0)
+    slider.value = Number.parseFloat(5.7)
+  }
+  else if(material == 'gabro'){   
+    slider.min = Number.parseFloat(6.5)      
+    slider.max = Number.parseFloat(7.0)
+    slider.value = Number.parseFloat(6.7)
+  }
+  else if(material == 'rochasultra'){   
+    slider.min = Number.parseFloat(7.5)      
+    slider.max = Number.parseFloat(8.5)
+    slider.value = Number.parseFloat(8.0)
+  }
+  else if(material == 'serpentinito'){   
+    slider.min = Number.parseFloat(5.5)      
+    slider.max = Number.parseFloat(6.5)
+    slider.value = Number.parseFloat(6.0)
+  }
+  
+  valueRange.innerHTML = slider.value;
+  
+  slider.oninput = function() {
+    valueRange.innerHTML = this.value;
+  }
+}
+
+
 
