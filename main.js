3ed67a612425d0b27d96f38b6f9a7b5a68ed8b3d
mcosta21 <marcioc424@gmail.com>
2019-11-26 13:21:28 -0300

    Criação de menu e inserção de logo

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { app, BrowserWindow } = require('electron')
+
+require('electron-reload')(__dirname, {
+  // Note that the path to electron may vary according to the main file
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
+function createWindow () {
+  // Cria uma janela de navegação.
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  // e carregar o index.html do aplicativo.
+  win.loadFile('index.html')
+}
+
+app.on('ready', createWindow)
